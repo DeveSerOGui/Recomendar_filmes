989c075ad122bafb8693121e34d8c75c24f4ae50
DeveSerOGui <guifelixs@gmail.com>
2023-06-07 17:35:53 -0300

    Alterando a cor do botão ao clicar no perguntas 1

--- app/src/main/java/br/com/comptar/guilhermeap2/PerguntasActivity.kt
+++ app/src/main/java/br/com/comptar/guilhermeap2/PerguntasActivity.kt
@@ -1,6 +1,7 @@
 package br.com.comptar.guilhermeap2
 
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -10,9 +11,33 @@ class PerguntasActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_perguntas)
 
+        val btnComedia : Button = findViewById(R.id.RespComedia)
+        btnComedia.setOnClickListener {
+            btnComedia.setBackgroundColor(Color.YELLOW)
+            btnComedia.setTextColor(Color.BLACK)
+        }
+
+        val btnAcao : Button = findViewById(R.id.RespAcao)
+        btnAcao.setOnClickListener {
+            btnAcao.setBackgroundColor(Color.YELLOW)
+            btnAcao.setTextColor(Color.BLACK)
+        }
+
+        val btnFicCien : Button = findViewById(R.id.RespFicCien)
+        btnFicCien.setOnClickListener {
+            btnFicCien.setBackgroundColor(Color.YELLOW)
+            btnFicCien.setTextColor(Color.BLACK)
+        }
+
+        val btnTerror : Button = findViewById(R.id.RespTerror)
+        btnTerror.setOnClickListener {
+            btnTerror.setBackgroundColor(Color.YELLOW)
+            btnTerror.setTextColor(Color.BLACK)
+        }
+
         val btGoPerguntas2 = findViewById<Button>(R.id.BtnConfirmar)
         btGoPerguntas2.setOnClickListener {
-            val intent = Intent(this@PerguntasActivity,Perguntas2Activity::class.java)
+            val intent = Intent(this@PerguntasActivity, Perguntas2Activity::class.java)
             startActivity(intent)
         }
     }
