abe197cec8545521488c94a34fa325efc273f2de
DeveSerOGui <guifelixs@gmail.com>
2023-06-13 16:58:40 -0300

    Tentando aplicar Intent na logo do IMDB

--- app/src/main/java/br/com/comptar/guilhermeap2/ResultadoActivity.kt
+++ app/src/main/java/br/com/comptar/guilhermeap2/ResultadoActivity.kt
@@ -1,7 +1,10 @@
 package br.com.comptar.guilhermeap2
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 
 class ResultadoActivity : AppCompatActivity() {
@@ -12,6 +15,14 @@ class ResultadoActivity : AppCompatActivity() {
         val nomeInformado = intent.getStringExtra("name")
         val nome : TextView = findViewById(R.id.Username)
         nome.text = "$nomeInformado"
+
+
+        //var btGoIMDB = findViewById<Button>(R.id.ButtonIMBD)
+        //btGoIMDB.setOnClickListener {
+        //    val url = Uri.parse("https://www.imdb.br")
+        //    val intent = Intent(Intent.ACTION_VIEW, url)
+        //    startActivity(intent)
+        //}
 
         //var ResultadoGenero : String = PerguntasActivity().Genero
 
